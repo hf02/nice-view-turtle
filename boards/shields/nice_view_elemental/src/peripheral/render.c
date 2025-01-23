02a16323b8ba02d0540002e5bd217c17bff50387
hf02 <55464333+hf02@users.noreply.github.com>
2025-01-22 21:00:25 -0500

    move batery

--- boards/shields/nice_view_elemental/src/peripheral/render.c
+++ boards/shields/nice_view_elemental/src/peripheral/render.c
@@ -14,7 +14,7 @@
 void render_battery() {
     lv_canvas_fill_bg(battery_canvas, BACKGROUND_COLOR, LV_OPA_COVER);
 
-    draw_battery(battery_canvas, 7, 4, states.battery);
+    draw_battery(battery_canvas, 7 , 4, states.battery);
 }
 
 void render_connectivity() {
