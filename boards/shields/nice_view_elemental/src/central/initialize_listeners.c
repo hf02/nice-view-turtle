f854c69158b44f20ea2282df7668ee8d8d771647
hf02 <git.ltx9l@slmail.me>
2025-01-24 17:39:25 -0500

    everything else

--- boards/shields/nice_view_elemental/src/central/initialize_listeners.c
+++ boards/shields/nice_view_elemental/src/central/initialize_listeners.c
@@ -22,40 +22,47 @@
 struct states states;
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_ELEMENTAL_ANIMATION)
-static void background_update_timer(lv_timer_t* timer)
+static void background_update_timer(lv_timer_t *timer)
 {
     states.background_index = (states.background_index + 1) % UINT_MAX;
 
     render_main();
 }
 
-lv_timer_t * timer;
+lv_timer_t *timer;
 
-static void start_timer() {
+static void start_timer()
+{
     // Call the `background_update_timer` function every configured interval.
     timer = lv_timer_create(background_update_timer, CONFIG_NICE_VIEW_ELEMENTAL_ANIMATION_FRAME_MS, NULL);
 }
 
 // We want to pause the animation when the keyboard is idling.
-int activity_update_callback(const zmk_event_t* eh) {
-    struct zmk_activity_state_changed* ev = as_zmk_activity_state_changed(eh);
-    if (ev == NULL) {
+int activity_update_callback(const zmk_event_t *eh)
+{
+    struct zmk_activity_state_changed *ev = as_zmk_activity_state_changed(eh);
+    if (ev == NULL)
+    {
         return -ENOTSUP;
     }
 
-    switch (ev->state) {
-        case ZMK_ACTIVITY_ACTIVE: {
-            lv_timer_resume(timer);
-            break;
-        }
-        case ZMK_ACTIVITY_IDLE:
-        case ZMK_ACTIVITY_SLEEP: {
-            lv_timer_pause(timer);
-            break;
-        }
-        default: {
-            return -EINVAL;
-        }
+    switch (ev->state)
+    {
+    case ZMK_ACTIVITY_ACTIVE:
+    {
+        lv_timer_resume(timer);
+        break;
+    }
+    case ZMK_ACTIVITY_IDLE:
+    case ZMK_ACTIVITY_SLEEP:
+    {
+        lv_timer_pause(timer);
+        break;
+    }
+    default:
+    {
+        return -EINVAL;
+    }
     }
 
     return 0;
@@ -65,24 +72,24 @@ int activity_update_callback(const zmk_event_t* eh) {
 // subscription. When subscribed, `activity_update_callback` will be called.
 ZMK_LISTENER(
     activity_update,
-    activity_update_callback
-);
+    activity_update_callback);
 
 // Subscribe the `activity_update` listener to the `zmk_activity_state_changed`
 // event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     activity_update,
-    zmk_activity_state_changed
-);
+    zmk_activity_state_changed);
 #endif
 
-static void battery_state_update_callback(struct battery_state state) {
+static void battery_state_update_callback(struct battery_state state)
+{
     states.battery = state;
 
     render_battery();
 }
 
-static struct battery_state get_battery_state(const zmk_event_t* event) {
+static struct battery_state get_battery_state(const zmk_event_t *event)
+{
     // `as_zmk_battery_state_changed(event)->state_of_charge` seems to be
     // usually used but the same value can be retrieved from the following.
     const uint8_t level = zmk_battery_state_of_charge();
@@ -103,32 +110,31 @@ ZMK_DISPLAY_WIDGET_LISTENER(
     struct battery_state,
     // Called after `get_battery_state` with the value it returned.
     battery_state_update_callback,
-    get_battery_state
-)
+    get_battery_state)
 
 // Subscribe the `widget_battery_state_update` listener to the
 // `zmk_battery_state_changed` event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     widget_battery_state_update,
     // Triggered when the computed battery percentage has changed.
-    zmk_battery_state_changed
-);
+    zmk_battery_state_changed);
 
 // Subscribe the `widget_battery_state_update` listener to the
 // `zmk_usb_conn_state_changed` event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     widget_battery_state_update,
     // Triggered when connected or disconnected from USB.
-    zmk_usb_conn_state_changed
-);
+    zmk_usb_conn_state_changed);
 
-static void connectivity_state_update_callback(struct connectivity_state state) {
+static void connectivity_state_update_callback(struct connectivity_state state)
+{
     states.connectivity = state;
 
     render_connectivity();
 }
 
-static struct connectivity_state get_connectivity_state(const zmk_event_t* event) {
+static struct connectivity_state get_connectivity_state(const zmk_event_t *event)
+{
     const struct zmk_endpoint_instance selected_endpoint = zmk_endpoints_selected();
     const int active_profile_index = zmk_ble_active_profile_index();
     const bool active_profile_connected = zmk_ble_active_profile_is_connected();
@@ -151,96 +157,120 @@ ZMK_DISPLAY_WIDGET_LISTENER(
     struct connectivity_state,
     // Called after `get_connectivity_state` with the value it returned.
     connectivity_state_update_callback,
-    get_connectivity_state
-)
+    get_connectivity_state)
 
 // Subscribe the `widget_connectivity_state_update` listener to the
 // `zmk_endpoint_changed` event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     widget_connectivity_state_update,
-    zmk_endpoint_changed
-);
+    zmk_endpoint_changed);
 
 // Subscribe the `widget_connectivity_state_update` listener to the
 // `zmk_usb_conn_state_changed` event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     widget_connectivity_state_update,
     // Triggered when connected or disconnected from USB.
-    zmk_usb_conn_state_changed
-);
+    zmk_usb_conn_state_changed);
 
 // Subscribe the `widget_connectivity_state_update` listener to the
 // `zmk_ble_active_profile_changed` event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     widget_connectivity_state_update,
     // Triggered when the selected profile has changed.
-    zmk_ble_active_profile_changed
-);
+    zmk_ble_active_profile_changed);
 
 bool was_layers_defined = false;
 
-static struct layer_state null_layer_state() {
+static struct layer_state null_layer_state()
+{
     struct layer_state state = {
         .index = -1,
-        .name = "(null)"
+        .name = "(null)",
+        .is_null = true,
     };
 
     return state;
 }
 
-static bool is_null_layer_state(struct layer_state state) {
-    return state.index == -1;
+static bool is_null_layer_state(struct layer_state state)
+{
+    return state.is_null;
 }
 
-static void initialize_layers_state() {
-    int array_size = sizeof(states.layers)/sizeof(struct layer_state);
-    for (int i = 0; i < array_size; i++) {
+static void initialize_layers_state()
+{
+    printf("initalizing states.layers!!");
+
+    int array_size = sizeof(states.layers) / sizeof(struct layer_state);
+    for (int i = 0; i < array_size; i++)
+    {
         states.layers[i] = null_layer_state();
     }
 }
 
-static void layer_state_update_callback(struct layer_state state) {
+static void layer_state_update_callback(struct layer_state state)
+{
+    printf("layer_state_update_callback() BEGIN\n");
     states.layer = state;
 
-    if (!was_layers_defined) {
+    printf("!was_layers_defined? %i\n", was_layers_defined);
+    if (!was_layers_defined)
+    {
         initialize_layers_state();
         was_layers_defined = true;
     }
 
-    int array_size = sizeof(states.layers)/sizeof(struct layer_state);
+    int array_size = sizeof(states.layers) / sizeof(struct layer_state);
+    printf("array_size = %i\n", array_size);
     bool should_clear_layers = false;
-    for (int i = 0; i < array_size; i++) {
-        if (should_clear_layers) {
+    for (int i = 0; i < array_size; i++)
+    {
+        printf("    index %i\n", i);
+        printf("        layer index: %i\n", states.layers[i].index);
+
+        if (should_clear_layers)
+        {
             states.layers[i] = null_layer_state();
-            continue;
-        }
-        
-        if (is_null_layer_state(states.layers[i])) {
-            states.layers[i] = state;
-            should_clear_layers = true;
-            states.layer_depth = i;
+            printf("...clearing index %i...\n", i);
             continue;
         }
 
-        if (states.layers[i].index == state.index) {
+        if (is_null_layer_state(states.layers[i]))
+        {
+            states.layers[i] = state;
             should_clear_layers = true;
-            states.layer_depth = i;
+            states.layer_depth = i + 1;
+            printf("index %i is null! so we set it to us :D\n", i);
+            printf("states.layer_depth = %i\n", i);
             continue;
         }
+
+        if (states.layers[i].index == state.index)
+        {
+            should_clear_layers = true;
+            states.layer_depth = i + 1;
+            printf("index %i is matches this layer! so we just start bombin\' :D\n", i);
+            printf("states.layer_depth = %i\n", i);
+            continue;
+        }
+
+        printf("index %i is like not important to us\n", i);
     }
 
     render_main();
+    printf("layer_state_update_callback() END\n");
 }
 
 // Retrieve the data we want from the event
-static struct layer_state get_layer_state(const zmk_event_t* event) {
+static struct layer_state get_layer_state(const zmk_event_t *event)
+{
     const zmk_keymap_layer_index_t index = zmk_keymap_highest_layer_active();
-    const char* name = zmk_keymap_layer_name(zmk_keymap_layer_index_to_id(index));
+    const char *name = zmk_keymap_layer_name(zmk_keymap_layer_index_to_id(index));
 
     struct layer_state state = {
         .index = index,
-        .name = name
-    };
+        .name = name,
+        .is_null = false};
 
     return state;
 }
@@ -252,18 +282,17 @@ ZMK_DISPLAY_WIDGET_LISTENER(
     struct layer_state,
     // Called after `get_layer_state` with the value it returned.
     layer_state_update_callback,
-    get_layer_state
-)
+    get_layer_state)
 
 // Subscribe the `widget_layer_state_update` listener to the
 // `zmk_battery_state_changed` event dispatched by ZMK.
 ZMK_SUBSCRIPTION(
     widget_layer_state_update,
     // Triggered when the selected layer has changed.
-    zmk_layer_state_changed
-);
+    zmk_layer_state_changed);
 
-void initialize_listeners() {
+void initialize_listeners()
+{
     widget_layer_state_update_init();
     widget_connectivity_state_update_init();
     widget_battery_state_update_init();
--- boards/shields/nice_view_elemental/src/central/render.c
+++ boards/shields/nice_view_elemental/src/central/render.c
@@ -23,26 +23,35 @@
 #include "../../include/images/background_temp_layer.h"
 #include "../../include/images/background_main_layer.h"
 
-void render_battery() {
+void render_battery()
+{
     lv_canvas_fill_bg(battery_canvas, BACKGROUND_COLOR, LV_OPA_COVER);
 
     draw_battery(battery_canvas, 5, 0, states.battery);
 }
 
-static void render_bluetooth_logo() {
+static void render_bluetooth_logo()
+{
     static const unsigned x = CONNECTIVITY_CANVAS_WIDTH - 12;
-    if (states.connectivity.active_profile_bonded) {
-        if (states.connectivity.active_profile_connected) {
+    if (states.connectivity.active_profile_bonded)
+    {
+        if (states.connectivity.active_profile_connected)
+        {
             draw_bluetooth_logo(connectivity_canvas, x, 0);
-        } else {
+        }
+        else
+        {
             draw_bluetooth_logo_outlined(connectivity_canvas, x, 0);
         }
-    } else {
+    }
+    else
+    {
         draw_bluetooth_searching(connectivity_canvas, x, 0);
     }
 }
 
-static void render_bluetooth_profile_index() {
+static void render_bluetooth_profile_index()
+{
     lv_draw_label_dsc_t label_dsc;
     lv_draw_label_dsc_init(&label_dsc);
     label_dsc.color = FOREGROUND_COLOR;
@@ -53,38 +62,46 @@ static void render_bluetooth_profile_index() {
     static const unsigned padding_y = -1;
     static const unsigned width = CONNECTIVITY_CANVAS_WIDTH - 18;
     static const char bluetooth_profile_label[5][2] = {"1", "2", "3", "4", "5"};
-    const char* label = bluetooth_profile_label[states.connectivity.active_profile_index];
-   
-    lv_canvas_draw_text(connectivity_canvas, 4, padding_y, width, &label_dsc, label); 
+    const char *label = bluetooth_profile_label[states.connectivity.active_profile_index];
+
+    lv_canvas_draw_text(connectivity_canvas, 4, padding_y, width, &label_dsc, label);
 }
 
-static void render_bluetooth_connectivity() {
+static void render_bluetooth_connectivity()
+{
     render_bluetooth_logo();
     render_bluetooth_profile_index();
 }
 
-static void render_usb_connectivity() {
+static void render_usb_connectivity()
+{
     draw_usb_logo(connectivity_canvas, 15, 4);
 }
 
-void render_connectivity() {
+void render_connectivity()
+{
     lv_canvas_fill_bg(connectivity_canvas, BACKGROUND_COLOR, LV_OPA_COVER);
 
-    switch (states.connectivity.selected_endpoint.transport) {
-        case ZMK_TRANSPORT_BLE: {
-            render_bluetooth_connectivity();
-            break;
-        }
-        case ZMK_TRANSPORT_USB: {
-            render_usb_connectivity();
-            break;
-        }
+    switch (states.connectivity.selected_endpoint.transport)
+    {
+    case ZMK_TRANSPORT_BLE:
+    {
+        render_bluetooth_connectivity();
+        break;
+    }
+    case ZMK_TRANSPORT_USB:
+    {
+        render_usb_connectivity();
+        break;
+    }
     }
 
     rotate_connectivity_canvas();
 }
 
-void render_main() {
+void render_main()
+{
+
 #if IS_ENABLED(CONFIG_NICE_VIEW_ELEMENTAL_BACKGROUND)
     // Unfortunately, text transparency does not seem to work in LVGL 8.3. This
     // forces us to redraw the background on every render instead of having it
@@ -96,41 +113,24 @@ void render_main() {
 
     lv_canvas_fill_bg(main_canvas, BACKGROUND_COLOR, LV_OPA_COVER);
 
-    lv_draw_img_dsc_t img_dsc;
-    lv_draw_img_dsc_init(&img_dsc);
-    switch (states.layer.index) {
-        case 0:
-            lv_canvas_draw_img(main_canvas, 0, 0, &background_main_layer, &img_dsc);
-            break;
-        case 7:
-        case 11:
-        // layers you use for a long time. ex: app specific, video, gaming
-            lv_canvas_draw_img(main_canvas, 0, 0, &background_alt_layer, &img_dsc);
-            break;
-        default:
-        // layers that you don't stick in for long. ex: symbols, portals, board management
-            lv_canvas_draw_img(main_canvas, 0, 0, &background_temp_layer, &img_dsc);
-
-    }
-
-    // Capitalize the layer name if given or use the layer number otherwise.
-    char* text = NULL;
-    
-    if (states.layer.name == NULL) {
-        text = malloc(10 * sizeof(char));
-        sprintf(text, "LAYER %i", states.layer.index);
-    }
-    else {
-        text = malloc((strlen(states.layer.name) + 1) * sizeof(char));
-        for (unsigned i = 0; states.layer.name[i] != '\0'; i++) {
-            text[i] = toupper(states.layer.name[i]);
-        }
-        text[strlen(states.layer.name)] = '\0';
-    }
+    // switch (states.layer.index)
+    // {
+    // case 0:
+    //     lv_canvas_draw_img(main_canvas, 0, 0, &background_main_layer, &img_dsc);
+    //     break;
+    // case 7:
+    // case 11:
+    //     // layers you use for a long time. ex: app specific, video, gaming
+    //     lv_canvas_draw_img(main_canvas, 0, 0, &background_alt_layer, &img_dsc);
+    //     break;
+    // default:
+    //     // layers that you don't stick in for long. ex: symbols, portals, board management
+    //     lv_canvas_draw_img(main_canvas, 0, 0, &background_temp_layer, &img_dsc);
+    // }
 
     // // Magic number. The height of the font from the baseline to the ascender
     // // height is 34px, but halving the space remaining of the full height gives
-    // // us another value ((68px - 34px) / 2 = 17px). 
+    // // us another value ((68px - 34px) / 2 = 17px).
     // static const unsigned text_y_offset = 15;
 
     // screen width
@@ -139,32 +139,75 @@ void render_main() {
     // V      V      V
     // 68px - 39px + 6px = 29px
     static const unsigned text_y_offset = 35;
+    const unsigned text_y_shift = text_y_offset - 2;
 
+    printf("-----------------\nimma rendering!\n\n");
+    printf("states.layer_depth = %i\n", states.layer_depth);
 
+    for (int i = 0; i < states.layer_depth; i++)
+    {
+        int reverse_index = states.layer_depth - (i + 1);
 
-    lv_draw_label_dsc_t layer_name_dsc;
-    lv_draw_label_dsc_init(&layer_name_dsc);
-    layer_name_dsc.color = FOREGROUND_COLOR;
-    layer_name_dsc.font = &pixel_operator_36;
-    layer_name_dsc.align = LV_TEXT_ALIGN_LEFT;
+        if (reverse_index >= 3)
+        {
+            printf("shit's gonna be off screen, so just skip it\n");
+            continue;
+        }
 
-    int array_size = sizeof(states.layers)/sizeof(struct layer_state);
-    for (int i = 0; i < array_size; i++) {
-        // int y_position = text_y_offset + (-text_y_offset * i);
+        printf("\nBEGIN LOOP!\n");
+        int y_position = text_y_offset - (text_y_shift * reverse_index);
+        printf("y_position = %i\n", y_position);
+
+        printf("this layer\'s text should be: ");
+        printf(states.layers[i].name);
+        printf("\n");
+
+        // Capitalize the layer name if given or use the layer number otherwise.u
+        char *text = NULL;
+
+        if (states.layers[i].name == NULL)
+        {
+            text = malloc(10 * sizeof(char));
+            sprintf(text, "Layer %i", states.layers[i].index);
+        }
+        else
+        {
+            text = malloc((strlen(states.layers[i].name) + 1) * sizeof(char));
+            for (unsigned j = 0; states.layers[i].name[j] != '\0'; j++)
+            {
+                text[j] = toupper(states.layers[i].name[j]);
+            }
+            text[strlen(states.layers[i].name)] = '\0';
+        }
+
+        printf("text: ");
+        printf(text);
+        printf("\n");
+
+        lv_draw_label_dsc_t layer_name_dsc;
+        lv_draw_label_dsc_init(&layer_name_dsc);
+        layer_name_dsc.color = FOREGROUND_COLOR;
+        layer_name_dsc.font = &pixel_operator_36;
+        layer_name_dsc.align = LV_TEXT_ALIGN_LEFT;
 
         lv_canvas_draw_text(
             main_canvas,
             -3,
-            text_y_offset,
+            y_position,
             MAIN_CANVAS_WIDTH,
             &layer_name_dsc,
-            text
-        );
+            text);
 
-        break;
+        free(text);
+        text = NULL;
+
+        printf("end loop...!\n\n");
     }
 
+    lv_draw_img_dsc_t img_dsc;
+    lv_draw_img_dsc_init(&img_dsc);
 
-    free(text);
-    text = NULL;
+    img_dsc.recolor = BACKGROUND_COLOR;
+
+    lv_canvas_draw_img(main_canvas, 0, 0, &background_main_layer, &img_dsc);
 }
